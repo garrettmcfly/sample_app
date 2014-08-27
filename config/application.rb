b05f13dc541c24fcb7dd9a4b74623b4e786a992f
garrett <garrett@garrett-desktop.(none)>
2014-08-26 21:48:11 -0400

    5.1 added bootstrap stylesheets

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,8 @@ Bundler.require(*Rails.groups)
 
 module SampleApp
   class Application < Rails::Application
+    
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -25,6 +27,6 @@ module SampleApp
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    # config.i18n.default_locale = :de    
   end
 end
