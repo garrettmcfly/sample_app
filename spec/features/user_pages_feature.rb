cc6c9bc8c3a12b0dcef3cc6a7cc2213aebb43865
garrett <garrett@garrett-desktop.(none)>
2014-08-29 19:21:20 -0400

    Chap 8 User Logins railstutorial.org

--- spec/features/user_pages_feature.rb
+++ spec/features/user_pages_feature.rb
@@ -1,21 +1,21 @@
 require 'rails_helper'
 
 describe "User pages" do
-	subject { page }
-	
-	describe "profile page" do
-	  let(:user) { FactoryGirl.create(:user) }
-	  before { visit user_path(user) }
-	  
-	  it { should have_content(user.name) }
-	  it { should have_title(user.name) }	  
-	end
-	
-	describe "signup page" do
-	  before { visit signup_path }
-	  
-	  it { should have_content("Sign up") }
-	  it { should have_title("Sign up") }
+  subject { page }
+  
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
+    
+    it { should have_content(user.name) }
+    it { should have_title(user.name) }   
+  end
+  
+  describe "signup page" do
+    before { visit signup_path }
+    
+    it { should have_content("Sign up") }
+    it { should have_title("Sign up") }
   end
   
   describe "signup" do
@@ -39,7 +39,16 @@ describe "User pages" do
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
+      
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: "user@example.com") }
+        
+        it { should have_link('Sign out') }
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      end      
     end
   end
 end
-		
+    
